b93b7b449376c31f463ea4cebd8b78d7505d8dd2
ILeonnard <igorleonnard@gmail.com>
2024-07-27 22:24:20 -0300

    Initial commit

---
+++ src/main/java/com/ileonnard/api_restful_java/ApiRestfulJavaApplication.java
@@ -0,0 +1,13 @@
+package com.ileonnard.api_restful_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ileonnard/api_restful_java/ApiRestfulJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ileonnard.api_restful_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
